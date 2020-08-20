397906d0f63f836635bbee9ebde78818779b128a
Kevin <k120435@icloud.com>
2020-08-20 16:00:05 +0800

    完成圖片上傳

---
+++ src/components/Calendar.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="calendar">
+    <div>{{ msg }}</div>
+    <table>
+      <thead>
+        <tr>
+          <th style="">123</th>
+          <th style="">123</th>
+          <th style="">123</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>2</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Calendar",
+  props: {
+    msg: String,
+  },
+  data: function() {
+    return {
+
+    }
+  },
+  mounted: function getOrderTime() {
+    axios
+      .get("https://l-beta.styleup.biz/fakeapi.php")
+      .then(function (response) {
+        console.log("succes");
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log("Errr");
+        console.log(error);
+      });
+  },
+  methods: {
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+  table {
+    width: 100%;
+    border-collapse: collapse;
+    border-spacing: 0;
+    border: 1px solid black;
+    font-size: 1em;
+    th,
+    tr,
+    td {
+      border: 1px solid black;
+    }
+  }
+</style>
---
+++ src/components/Uploads.vue
@@ -0,0 +1,105 @@
+<template>
+  <div id="uploads">
+    <div v-for="(upload,index) in uploads" :key="index" class="upload">
+      <div class="upload_title">{{ upload.title }}</div>
+      <div class="upload_preview">
+        <img v-for="(preview,index) in upload.preview_list" :key="index" :src="preview" alt="" class="preview_img">
+      </div>
+      <div @click="pickImage" class="upload_btn" :data-type="upload.type">{{ upload.btnText }}</div>
+      <input @change="previewMultiImage" :data-type="upload.type" type="file" accept="image/jpg, image/png" multiple="multiple" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Uploads",
+  props: {
+    msg: String,
+  },
+  data: function () {
+    return {
+      uploads: [
+        { title: "服務前造型", btnText: "上傳圖片", type: "before", preview_list: []},
+        { title: "服務前造型", btnText: "上傳圖片", type: "after", preview_list: [] },
+      ],
+      image_list: [],
+    };
+  },
+  methods: {
+    pickImage: function (event) {
+      const uploadType = event.target.dataset.type
+      if(uploadType === 'before'){
+        this.uploads[0].preview_list = []
+      }else {
+        this.uploads[1].preview_list = []
+      }
+      event.target.nextElementSibling.click();
+    },
+    previewMultiImage: function (event) {
+      const input = event.target;
+      const inputType = event.target.dataset.type;
+      let count = input.files.length;
+      let index = 0;
+      if (input.files) {
+        while (count--) {
+          const reader = new FileReader();
+          reader.onload = (e) => {
+            if(inputType === 'before'){
+              this.uploads[0].preview_list.push(e.target.result);
+            }else {
+              this.uploads[1].preview_list.push(e.target.result);
+            }
+          };
+          this.image_list.push(input.files[index]);
+          reader.readAsDataURL(input.files[index]);
+          index++;
+        }
+      }
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+#uploads {
+  display: flex;
+  flex-direction: column;
+  background-color: #f2f2f4;
+  padding: 10px;
+  .upload {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+    height: 50px;
+    border-top: 1px solid #e0e0e0;
+    padding: 10px 0;
+
+    &:last-child {
+      border-bottom: 1px solid #e0e0e0;
+    }
+
+    &_title {
+      color: #727272;
+    }
+    &_preview {
+      flex: 1 calc(100% - 200px);
+      display: flex;
+      overflow: hidden;
+      .preview_img {
+        height: 50px;
+      }
+    }
+    &_btn {
+      background-color: #020022;
+      border-radius: 10px;
+      color: #ffffff;
+      padding: 2px 20px;
+    }
+    input {
+      display: none;
+    }
+  }
+}
+</style>
